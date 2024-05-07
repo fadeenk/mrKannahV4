1cb9e2e3c41fc497bc856a37fe89eaeb9c66798a
Fadee Kannah <fadeekannah@gmail.com>
2024-05-06 22:03:21 -0700

    Add years page

---
+++ src/pages/years.tsx
@@ -0,0 +1,43 @@
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import { useState } from 'react';
+
+const years = {
+    2023: 'eventful',
+    2022: 'traveling',
+    2021: 'vegas',
+    2020: 'covid',
+}
+
+export default function Home(): JSX.Element {
+    const {siteConfig} = useDocusaurusContext();
+    const [attemptedPass, setAttemptedPass] = useState<string>();
+  
+    if ('facebook' !== attemptedPass){
+      return ( 
+          <input type="text" className="passwordProtectedDoc" 
+          placeholder="Enter the password" 
+          onChange={(e) => setAttemptedPass(e.target.value)}
+          />
+      )
+    } 
+  
+    return (
+      <Layout
+        title={`Yearly reviews links`}
+        description={`${siteConfig.tagline}`}>
+        <div className='container'>
+            {
+                Object.keys(years).reverse().map((year) => (
+                <div className='card'>
+                    <div className='card__body'>
+                        <p><a href={"./blog/Yearly/"+year}>{year}</a>: <b>{years[year]}</b></p>
+                    </div>
+                </div>
+                ))
+            }
+        </div>
+      </Layout>
+    );
+  }
+  
